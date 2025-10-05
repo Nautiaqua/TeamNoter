1845551b04a3b60bc9ed1d9c846d6bce27396ca3
nautia <134142097+Nautiaqua@users.noreply.github.com>
2025-10-05 20:05:34 +0800

    Sidebar UI complete

---
+++ TeamNoter/Windows/Dashboard.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamNoter
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        bool sidebarExpanded;
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void expandBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!sidebarExpanded) // Sidebar ISN'T expanded
+            {
+                mainGrid.ColumnDefinitions[0].Width = new GridLength(200);
+                sidebarExpanded = true;
+            }
+            else // Sidebar IS expanded
+            {
+                mainGrid.ColumnDefinitions[0].Width = new GridLength(50);
+
+                sidebarExpanded = false;
+            }
+        }
+    }
+}
