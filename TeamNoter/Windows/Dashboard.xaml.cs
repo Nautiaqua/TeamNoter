85578de9a509b804faa2525977ec863b5e46f8e8
nautia <134142097+Nautiaqua@users.noreply.github.com>
2025-10-06 17:00:05 +0800

    Sidebar complete

--- TeamNoter/Windows/Dashboard.xaml.cs
+++ TeamNoter/Windows/Dashboard.xaml.cs
@@ -20,23 +20,53 @@ namespace TeamNoter
     public partial class Dashboard : Window
     {
         bool sidebarExpanded;
+        int expandedSize = 190;
+        int collapsedSize = 40;
         public Dashboard()
         {
             InitializeComponent();
         }
-
         private void expandBtn_Click(object sender, RoutedEventArgs e)
         {
             if (!sidebarExpanded) // Sidebar ISN'T expanded
             {
                 mainGrid.ColumnDefinitions[0].Width = new GridLength(200);
                 sidebarExpanded = true;
+
+                accLabel.Visibility = Visibility.Visible;
+                accBtn.Width = expandedSize;
+                
+                dbLabel.Visibility = Visibility.Visible;
+                dbBtn.Width = expandedSize;
+                
+                manageLabel.Visibility = Visibility.Visible;
+                manageBtn.Width = expandedSize;
+                
+                taskLabel.Visibility = Visibility.Visible;
+                taskBtn.Width = expandedSize;
+                
+                logoutLabel.Visibility = Visibility.Visible;
+                logoutBtn.Width = expandedSize;
             }
             else // Sidebar IS expanded
             {
                 mainGrid.ColumnDefinitions[0].Width = new GridLength(50);
-
                 sidebarExpanded = false;
+
+                accLabel.Visibility = Visibility.Hidden;
+                accBtn.Width = collapsedSize;
+
+                dbLabel.Visibility = Visibility.Hidden;
+                dbBtn.Width = collapsedSize;
+
+                manageLabel.Visibility = Visibility.Hidden;
+                manageBtn.Width = collapsedSize;
+
+                taskLabel.Visibility = Visibility.Hidden;
+                taskBtn.Width = collapsedSize;
+
+                logoutLabel.Visibility = Visibility.Hidden;
+                logoutBtn.Width = collapsedSize;
             }
         }
     }
