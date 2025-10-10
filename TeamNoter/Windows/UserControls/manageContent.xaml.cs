c7cb3327c77deb2efcb806a7a137442a6bd5b268
Joaquin Antonio Alcid <joaquinalcid2005@gmail.com>
2025-10-10 22:25:30 +0800

    new task design UI

--- TeamNoter/Windows/UserControls/manageContent.xaml.cs
+++ TeamNoter/Windows/UserControls/manageContent.xaml.cs
@@ -24,5 +24,10 @@ namespace TeamNoter.Windows.UserControls
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
